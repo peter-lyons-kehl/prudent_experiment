0ba239d3266f9c453f40b53422aae11a7697f795
Peter Lyons Kehl <peter.kehl@gmail.com>
2025-12-05 16:16:28 -0800

    unsafe_fn for qualified generic functions/methods/associated functions

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@
 
 pub const AFTER_PATH: &[char] = &['{', '[', ',', '>', '=', ':', ';', '|']; // and: "=>"
 */
+
 #[macro_export]
 macro_rules! unsafe_fn {
     ( $fn:path | $( $arg:expr ),+ ) => {
@@ -24,13 +25,9 @@ macro_rules! unsafe_fn {
           unsafe { /* * */ prudent_unique_local_function_alias( $( $arg ),+ ) }
         })
     };
-    // some::path::type>method_x
-    /*( $path:path : $type:ident > $method:ident $( $arg:expr ),+ ) => {
-        ({
-          use $path::{$type};
-          unsafe { /* ****** */ $type::$method( $( $arg ),+ ) }
-        })
-    };*/
+    ( $type_and_method:ty | $( $arg:expr ),+ ) => {
+          unsafe { /* ****** */ $type_and_method( $( $arg ),+ ) }
+    };
     ( $type:ty > $method:ident $( $arg:expr ),+ ) => {
           unsafe { /* ****** */ <$type>::$method( $( $arg ),+ ) }
     };
@@ -83,70 +80,95 @@ const GEN: Gen<u8> = Gen { t: 0 };
 struct Strc;
 impl Strc {
     const fn method(&self, _: u8, _: u8) {}
+    const fn method_gen<T>(&self, _: u8, _: u8) {}
     const fn associated_fn(_: i8, _: u64) {}
     const fn associated_gen<T>(_: u8, _: u8) {}
 }
+const S: Strc = Strc;
 #[rustfmt::skip]
 const _FN: () = {
     use core::primitive;
     let unchecked_add = u8::unchecked_add;
-    // - functions, methods, associated functions
-    // - paths only; not generic qualifications of methods/associated functions
+    // - functions/methods/associated functions
+    // - paths only; not specified generic qualifications of the receiver types and/or their
+    //   methods/associated functions
     unsafe_fn!(                     crate::f|       1, 2);
     unsafe_fn!(            u8::unchecked_add|       1, 2);
-    // - Same, but a comma instead of the pipe. TODO Remove?
+
+    // - functions/methods/associated functions
+    // - function/method/associated function is a result of any well-formed expression (including
+    //   generic qualifications of the receiver types and/or their methods/associated functions)
     unsafe_fn!(                     crate::f,       1, 2);
     unsafe_fn!(            u8::unchecked_add,       1, 2);
-    unsafe_fn!(          Strc::associated_fn,       1, 2);
+    unsafe_fn!(       Strc::method_gen::<i8>, &S,   1, 2);
     unsafe_fn!(   crate::Strc::associated_fn,       1, 2);
+    unsafe_fn!(   Strc::associated_gen::<()>,       1, 2);
+    unsafe_fn!(   Strc::associated_gen::<u8>,       1, 2);
+    unsafe_fn!(            Gen::<u8>::method, &GEN, 1, 2);
+
+    // - paths + function name only;
     // - functions only (not for methods/associated functions)
     unsafe_fn!(                     crate: f        1, 2);
 
-    // qualified methods and associated functions only, with a type (the type is in scope, or with a
-    // path)
-    unsafe_fn!(           u8  >unchecked_add        1, 2);
-    unsafe_fn!(primitive::u8  >unchecked_add        1, 2);
-    unsafe_fn!(       Gen<u8> >method         &GEN, 1, 2);
-    unsafe_fn!(crate::Gen<u8> >method         &GEN, 1, 2);
-    unsafe_fn!(          Strc >associated_fn        1, 2);
-    unsafe_fn!(   crate::Strc >associated_fn        1, 2);
+    // - qualified methods/associated functions only
+    // - with a specified receiver type (the type must be in scope, or with a path)
+    // - the receiver type may have generic qualifications, but
+    // - the method/associated function can't have any generic qualifications specified
+    unsafe_fn!(            u8  >unchecked_add        1, 2);
+    unsafe_fn!( primitive::u8  >unchecked_add        1, 2);
+    unsafe_fn!(        Gen<u8> >method         &GEN, 1, 2);
+    unsafe_fn!(    crate::Strc >associated_fn        1, 2);
 
-    // - function name (identifier) in scope
-    // - functions; methods/associated functions only if passed stored in a const/variable
-    unsafe_fn!(                unchecked_add        1, 2);
+    // - qualified functions/methods/associated functions only
+    // - with a specified receiver type (the type must be in scope, or with a path), and/or with
+    //   specified method's/associated function's generic parameters
+    unsafe_fn!(        Gen<u8>::method| &GEN, 1, 2);
+    unsafe_fn!(    crate::Strc::associated_gen<()>|        1, 2);
+    type _T = (Gen<u8>);
+    type _U = Gen::<u8>;
+    type _V = (Gen::<u8>);
 
-    // - function is a result of a block
-    // - functions, methods, associated functions
-    unsafe_fn!(           {u8::unchecked_add}       1, 2);
-    unsafe_fn!( {let m = u8::unchecked_add;
-                                           m}       1, 2);
-    unsafe_fn!(           {<Gen<u8>>::method} &GEN, 1, 2);
-    unsafe_fn!(         {Strc::associated_fn}       1, 2);
-    unsafe_fn!(  {Strc::associated_gen::<u8>}       1, 2);
+    // - function name (identifier) in scope - no path; no specified generic qualifications
+    // - functions; methods/associated functions only if passed in via a const/variable
+    unsafe_fn!(                 unchecked_add        1, 2);
+
+    // - function is a result of a block (its last expression; any well-formed expression)
+    // - functions/methods/associated functions
+    // - for methods/associated functions both the receiver type and the methods/associated
+    //   functions may have specified generic qualifications
+    unsafe_fn!(            {u8::unchecked_add}       1, 2);
+    unsafe_fn!(  {let m = u8::unchecked_add;
+                                            m}       1, 2);
+    unsafe_fn!(            {<Gen<u8>>::method} &GEN, 1, 2);
+    unsafe_fn!(          {Strc::associated_fn}       1, 2);
+    unsafe_fn!(   {Strc::associated_gen::<u8>}       1, 2);
 };
 
-const S: Strc = Strc;
 #[rustfmt::skip]
 const _MD: () = {
-    // - receiver passed in as a literal, or an identifier (const/variable), **and** in scope
+    // - receiver passed in as a literal/identifier (const/variable), **and** in scope - no path;
+    // - no specified generic qualifications
     unsafe_md!(      1u8  .unchecked_add 1   );
     unsafe_md!(        S  .method        1, 2);
 
-    // path::to::module :CONST_OR_STATIC
+    // - path::to::module :CONST_OR_STATIC
+    // - no specified generic qualifications
     unsafe_md!( crate: S  .method        1, 2);
 
-    // path::to::CONST_OR_STATIC
+    // - path::to::CONST_OR_STATIC
+    // - no specified generic qualifications
     unsafe_md!( crate::S  >method        1, 2);
 
-    // receiver is passed as an expression
+    // - receiver is passed as an expression
+    // - no specified generic qualifications
     unsafe_md!(      1u8 =>unchecked_add 2   );
     unsafe_md!(      *&S =>method        1, 2);
 
     // - receiver is an expression passed in between `{...}`. But, **unlike** unsafe_fn, here the
     //   receiver expression **cannot** be preceded by any number of statements (inside that
-    //   `{...}`).
+    //   `{...}`). The whole block must consist of exactly one expression.
     //
-    // Of course, you can put the expression in an extra pair of brackets `{...}`. But then the
-    // result will move, or it will have to be Copy and it will be copied.
+    // Of course, you can put any block in an extra pair of brackets `{...}`. But then the result
+    // will move, or it will have to be Copy and it will be copied.
     unsafe_md!(    {*&S}  .method        1, 2);
 };
